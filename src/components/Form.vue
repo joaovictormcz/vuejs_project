b8d0df83e19948cfcea0a53a3f18f5f65fee741f
joaovictormcz <joaovictor.mcz@gmail.com>
2023-08-13 14:49:49 -0300

    Aula 15

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
-        <form action="">
+        <form action="" @submit="enviarFormulario($event)">
             <h2>Meu formulário</h2>
             <div>
-                <InputText />
+                <input type="text" v-model="name">
             </div>
             <div>
-                <InputText />
+                <input type="text" v-model="email">
             </div>
             <div>
                 <Submit />
             </div>
         </form>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
     </div>
 </template>
 
@@ -24,6 +27,38 @@
         components: {
             InputText,
             Submit
+        },
+        data() {
+            return {
+                name: "",
+                email: ""
+            }
+
+        },
+        methods:{
+            enviarFormulario(e){
+
+                e.preventDefault();
+
+                const name = this.name;
+                const email = this.email;
+
+                console.log("Formulário Enviado!");
+                console.log("O nome é: " + name);
+                console.log("O email é: " + email);
+
+                // Ajax
+
+
+                // Inserir no banco os dados
+
+            }
         }
     }
 </script>
+
+<style scoped>
+    p {
+        margin: 150px;
+    }
+</style>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,11 +1,18 @@
 <template>
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{ nome }} e a minha profissão é {{profissao}}</p>
+    <MultiplosEventos />
 </template>
 
 <script>
+
+    import MultiplosEventos from './MultiplosEventos.vue';
+
     export default {
         name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
         data(){
             return {
                 nome: "Matheus",
