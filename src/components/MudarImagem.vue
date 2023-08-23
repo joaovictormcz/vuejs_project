1856662f3893fe6710dd52de7d55364b521ebc4a
joaovictormcz <joaovictor.mcz@gmail.com>
2023-08-23 19:58:02 -0300

    Aula 18

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button @click="$emit('mudarImagem')">
+            Trocar imagem de Perfil
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MudarImagem',
+        emits: ['mudarImagem']
+    }
+</script>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,18 +1,27 @@
 <template>
     <img :src="avatar" :alt="descricao">
-    <Reutilizacao />
+    <MudarImagem @mudar-imagem="trocarImagem" />
 </template>
 <script>
-    import Reutilizacao from './Reutilizacao.vue';
+    import MudarImagem from './MudarImagem.vue';
     export default {
         name: 'Picture',
-        components: {
-            Reutilizacao
+        components: {    
+            MudarImagem
         },
         data(){
             return {
                 avatar: "/img/avatar.png",
                 descricao: "João Victor"
+            }
+        },
+        methods: {
+            trocarImagem(){                
+                if (this.avatar == "/img/avatar2.png") {
+                    this.avatar = "/img/avatar.png";
+                } else {
+                    this.avatar = "/img/avatar2.png";
+                }
             }
         }
     }
